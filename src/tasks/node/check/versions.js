586e1d94a1eb248e4c89b074ebc74180f98dbcd8
Ourchitecture.io <97544811+ourchitectureio@users.noreply.github.com>
2023-11-02 03:44:52 -0400

    chore: test engines and packageManager
    Signed-off-by: Ourchitecture.io <97544811+ourchitectureio@users.noreply.github.com>

--- src/tasks/node/check/versions.js
+++ src/tasks/node/check/versions.js
@@ -1,5 +1,9 @@
 import { URL } from 'url'
 
+import Box from 'cli-box'
+import chalk from 'chalk'
+import latestVersion from 'latest-version'
+import semver from 'semver'
 import which from 'which'
 
 import * as cmd from '../../../libraries/node/cmd.js'
@@ -13,22 +17,23 @@ const cleanEngineVersion = (engineVersion) => {
     return engineVersion.replace(/[<=>]/g, '')
 }
 
-const main = async (scriptFilePath) => {
-    await host.loadDotenv()
+const checkNodeVersions = async (
+    nodePackageFilePath,
+    nodePackageInfo,
+    backstagePackageFilePath,
+    backstagePackageInfo
+) => {
+    if (!nodePackageInfo.engines || !nodePackageInfo.engines.node) {
+        const missingEngineError = `Missing expected configuration "engines.node" key in file "${nodePackageFilePath}".`
+        log.error(missingEngineError)
+        throw new Error(missingEngineError)
+    }
 
-    log.registerLoggerSingleton(scriptFilePath)
-
-    log.info('Checking versions...')
-
-    const results = []
-
-    const nodePackageFilePath = host.getRelativeToRootPath('./package.json')
-    const nodePackageInfo = await host.readJson(nodePackageFilePath)
-
-    const backstagePackageFilePath = host.getRelativeToRootPath(
-        './src/backstage/ourstage/package.json'
-    )
-    const backstagePackageInfo = await host.readJson(backstagePackageFilePath)
+    if (!backstagePackageInfo.engines || !backstagePackageInfo.engines.node) {
+        const missingEngineError = `Missing expected configuration "engines.node" key in file "${backstagePackageFilePath}".`
+        log.error(missingEngineError)
+        throw new Error(missingEngineError)
+    }
 
     const nvmVersionFilePath = host.getRelativeToRootPath('./.nvmrc')
     const expectedNvmVersion = (await host.readFile(nvmVersionFilePath))
@@ -103,6 +108,27 @@ const main = async (scriptFilePath) => {
         version "${actualNodeVersion}".`
         )
     }
+}
+
+const checkNpmVersions = async (
+    nodePackageFilePath,
+    nodePackageInfo,
+    backstagePackageFilePath,
+    backstagePackageInfo
+) => {
+    if (!nodePackageInfo.engines || !nodePackageInfo.engines.npm) {
+        const missingEngineError = `Missing expected configuration "engines.npm" key in file "${nodePackageFilePath}".`
+        log.error(missingEngineError)
+        throw new Error(missingEngineError)
+    }
+
+    if (!backstagePackageInfo.engines || !backstagePackageInfo.engines.npm) {
+        const missingEngineError = `Missing expected configuration "engines.npm" key in file "${backstagePackageFilePath}".`
+        log.error(missingEngineError)
+        throw new Error(missingEngineError)
+    }
+
+    const latestNpmVersion = await latestVersion('npm')
 
     const expectedNodePackageNpmVersion = cleanEngineVersion(
         nodePackageInfo.engines.npm
@@ -118,6 +144,7 @@ const main = async (scriptFilePath) => {
     ).trim()
 
     log.debug('npm command versions', {
+        latestNpmVersion,
         expectedNodePackageNpmVersion,
         expectedBackstagePackageNpmVersion,
         npmVersionFilePath,
@@ -125,6 +152,23 @@ const main = async (scriptFilePath) => {
         actualNpmCommandPath,
         actualNpmVersion,
     })
+
+    const isNewVersionAvailable = semver.gt(latestNpmVersion, actualNpmVersion)
+
+    if (isNewVersionAvailable) {
+        log.warn(
+            `Update available for npm! ${actualNpmVersion} → ${latestNpmVersion}`
+        )
+        const purple = chalk.hex('800080')
+        const updateBox = Box(
+            '60x5',
+            `Update available for npm! ${chalk.red(
+                actualNpmVersion
+            )} → ${chalk.green(latestNpmVersion)}\n
+            Run "${purple('npm install --location=global npm')}" to update.`
+        )
+        console.log(updateBox.toString())
+    }
 
     if (expectedNodePackageNpmVersion !== expectedNpmVersion) {
         log.error('Found incompatible npm versions between two files', {
@@ -135,8 +179,8 @@ const main = async (scriptFilePath) => {
         })
         throw new Error(
             `The npm versions do not match from the files "${nodePackageFilePath}"
-        with version "${expectedNodePackageNpmVersion}" and
-        "${npmVersionFilePath}" with version "${expectedNpmVersion}".`
+      with version "${expectedNodePackageNpmVersion}" and
+      "${npmVersionFilePath}" with version "${expectedNpmVersion}".`
         )
     }
 
@@ -149,9 +193,9 @@ const main = async (scriptFilePath) => {
         })
         throw new Error(
             `The npm versions do not match from the files "${nodePackageFilePath}"
-        with version "${expectedNodePackageNpmVersion}" and
-        "${backstagePackageFilePath}" with version
-        "${expectedBackstagePackageNpmVersion}".`
+      with version "${expectedNodePackageNpmVersion}" and
+      "${backstagePackageFilePath}" with version
+      "${expectedBackstagePackageNpmVersion}".`
         )
     }
 
@@ -164,14 +208,42 @@ const main = async (scriptFilePath) => {
         })
         throw new Error(
             `The expected npm version "${expectedNodePackageNpmVersion}"
-        does not match from "${nodePackageFilePath}" and the actual
-        version "${actualNpmVersion}".`
+      does not match from "${nodePackageFilePath}" and the actual
+      version "${actualNpmVersion}".`
         )
     }
+}
+
+const checkYarnVersions = async (
+    backstagePackageFilePath,
+    backstagePackageInfo
+) => {
+    if (!backstagePackageInfo.engines || !backstagePackageInfo.engines.yarn) {
+        const missingEngineError = `Missing expected configuration "engines.yarn" key in file "${backstagePackageFilePath}".`
+        log.error(missingEngineError)
+        throw new Error(missingEngineError)
+    }
+
+    if (!backstagePackageInfo.packageManager) {
+        const missingPackageManagerError = `Missing expected configuration "packageManager" key in file "${backstagePackageFilePath}".`
+        log.error(missingPackageManagerError)
+        throw new Error(missingPackageManagerError)
+    }
+
+    const latestYarnVersion = await latestVersion('yarn')
 
     const expectedBackstagePackageYarnVersion = cleanEngineVersion(
         backstagePackageInfo.engines.yarn
     )
+
+    const expectedNodePackageManager = `yarn@${expectedBackstagePackageYarnVersion}`
+
+    if (backstagePackageInfo.packageManager != expectedNodePackageManager) {
+        const unexpectedPackageManagerError = `Expected node configuration key "packageManager" to have a value of "${expectedNodePackageManager}" in file "${backstagePackageFilePath}".`
+        log.error(unexpectedPackageManagerError)
+        throw new Error(unexpectedPackageManagerError)
+    }
+
     const actualYarnCommandPath = (await which('yarn')).trim()
     const actualYarnVersion = (
         await cmd.runWithProgressAndExpectStdOutCommand('yarn', ['--version'])
@@ -183,6 +255,26 @@ const main = async (scriptFilePath) => {
         actualYarnVersion,
     })
 
+    const isNewVersionAvailable = semver.gt(
+        latestYarnVersion,
+        actualYarnVersion
+    )
+
+    if (isNewVersionAvailable) {
+        log.warn(
+            `Update available for yarn! ${actualYarnVersion} → ${latestYarnVersion}`
+        )
+        const purple = chalk.hex('800080')
+        const updateBox = Box(
+            '60x5',
+            `Update available for yarn! ${chalk.red(
+                actualYarnVersion
+            )} → ${chalk.green(latestYarnVersion)}\n
+            Run "${purple('npm install --location=global yarn')}" to update.`
+        )
+        console.log(updateBox.toString())
+    }
+
     if (expectedBackstagePackageYarnVersion !== actualYarnVersion) {
         log.error('Expected yarn version was not the actual version', {
             expectedBackstagePackageYarnVersion,
@@ -192,14 +284,39 @@ const main = async (scriptFilePath) => {
         })
         throw new Error(
             `The expected yarn version "${expectedBackstagePackageYarnVersion}"
-        does not match from "${backstagePackageFilePath}" and the actual
-        version "${actualYarnVersion}".`
+      does not match from "${backstagePackageFilePath}" and the actual
+      version "${actualYarnVersion}".`
         )
     }
+}
+
+const checkPnpmVersions = async (nodePackageFilePath, nodePackageInfo) => {
+    if (!nodePackageInfo.engines || !nodePackageInfo.engines.pnpm) {
+        const missingEngineError = `Missing expected configuration "engines.pnpm" key in file "${nodePackageFilePath}".`
+        log.error(missingEngineError)
+        throw new Error(missingEngineError)
+    }
+
+    if (!nodePackageInfo.packageManager) {
+        const missingPackageManagerError = `Missing expected configuration "packageManager" key in file "${nodePackageFilePath}".`
+        log.error(missingPackageManagerError)
+        throw new Error(missingPackageManagerError)
+    }
+
+    const latestPnpmVersion = await latestVersion('pnpm')
 
     const expectedNodePackagePnpmVersion = cleanEngineVersion(
         nodePackageInfo.engines.pnpm
     )
+
+    const expectedNodePackageManager = `pnpm@${expectedNodePackagePnpmVersion}`
+
+    if (nodePackageInfo.packageManager != expectedNodePackageManager) {
+        const unexpectedPackageManagerError = `Expected node configuration key "packageManager" to have a value of "${expectedNodePackageManager}" in file "${nodePackageFilePath}".`
+        log.error(unexpectedPackageManagerError)
+        throw new Error(unexpectedPackageManagerError)
+    }
+
     const pnpmVersionFilePath = host.getRelativeToRootPath('./.pnpmrc')
     const expectedPnpmVersion = (
         await host.readFile(pnpmVersionFilePath)
@@ -209,13 +326,33 @@ const main = async (scriptFilePath) => {
         await cmd.runWithProgressAndExpectStdOutCommand('pnpm', ['--version'])
     ).trim()
 
-    log.debug('npm command versions', {
+    log.debug('pnpm command versions', {
         expectedNodePackagePnpmVersion,
         pnpmVersionFilePath,
         expectedPnpmVersion,
         actualPnpmCommandPath,
         actualPnpmVersion,
     })
+
+    const isNewVersionAvailable = semver.gt(
+        latestPnpmVersion,
+        actualPnpmVersion
+    )
+
+    if (isNewVersionAvailable) {
+        log.warn(
+            `Update available for npm! ${actualPnpmVersion} → ${latestPnpmVersion}`
+        )
+        const purple = chalk.hex('800080')
+        const updateBox = Box(
+            '60x5',
+            `Update available for pnpm! ${chalk.red(
+                actualPnpmVersion
+            )} → ${chalk.green(latestPnpmVersion)}\n
+            Run "${purple('npm install --location=global pnpm')}" to update.`
+        )
+        console.log(updateBox.toString())
+    }
 
     if (expectedNodePackagePnpmVersion !== expectedPnpmVersion) {
         log.error('Found incompatible pnpm versions between two files', {
@@ -226,8 +363,8 @@ const main = async (scriptFilePath) => {
         })
         throw new Error(
             `The pnpm versions do not match from the files "${nodePackageFilePath}"
-        with version "${expectedNodePackagePnpmVersion}" and
-        "${pnpmVersionFilePath}" with version "${expectedPnpmVersion}".`
+      with version "${expectedNodePackagePnpmVersion}" and
+      "${pnpmVersionFilePath}" with version "${expectedPnpmVersion}".`
         )
     }
 
@@ -240,10 +377,44 @@ const main = async (scriptFilePath) => {
         })
         throw new Error(
             `The expected pnpm version "${expectedNodePackagePnpmVersion}" does not
-        match from "${nodePackageFilePath}" and the actual version
-        "${actualPnpmVersion}".`
+      match from "${nodePackageFilePath}" and the actual version
+      "${actualPnpmVersion}".`
         )
     }
+}
+
+const main = async (scriptFilePath) => {
+    await host.loadDotenv()
+
+    log.registerLoggerSingleton(scriptFilePath)
+
+    log.info('Checking versions...')
+
+    const nodePackageFilePath = host.getRelativeToRootPath('./package.json')
+    const nodePackageInfo = await host.readJson(nodePackageFilePath)
+
+    const backstagePackageFilePath = host.getRelativeToRootPath(
+        './src/backstage/ourstage/package.json'
+    )
+    const backstagePackageInfo = await host.readJson(backstagePackageFilePath)
+
+    await checkNodeVersions(
+        nodePackageFilePath,
+        nodePackageInfo,
+        backstagePackageFilePath,
+        backstagePackageInfo
+    )
+
+    await checkNpmVersions(
+        nodePackageFilePath,
+        nodePackageInfo,
+        backstagePackageFilePath,
+        backstagePackageInfo
+    )
+
+    await checkYarnVersions(backstagePackageFilePath, backstagePackageInfo)
+
+    await checkPnpmVersions(nodePackageFilePath, nodePackageInfo)
 
     log.info('Successfully checked versions.')
 }
